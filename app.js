90dba1b32467377a72d0863e1d1ea31b6d4d5b10
Karan Barsiwal <barsiwal.karan@tftus.com>
2017-10-27 15:45:28 +0530

    initial 2

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var request = require('request');
 
+var username = "admin@karan-barsiwal-linux";
+var password = "admin";
+var url = "http://localhost:5280/admin/server/karan-barsiwal-linux/users/";
+var auth = "Basic " + new Buffer(username + ":" + password).toString("base64");
 var app = express();
 
 app.use(bodyParser.json());
@@ -16,25 +20,53 @@ app.post("/", function (req, res) {
     var password;
     username = req.body.username;
     password = req.body.password;
+    PostCode();
     return res.send({
-        var request = require('request'),
-            username = "admin@karan-barsiwal-linux",
-            password = "admin",
-            url = "http://localhost:5280/admin/server/karan-barsiwal-linux/users/",
-            auth = "Basic " + new Buffer(username + ":" + password).toString("base64");
-
-        request({
-                url: url,
-                headers: {
-                    "Authorization": auth
-                }
-            },
-            function (error, response, body) {
-                // Do more stuff with 'body' here
-            }
-        );
+        status:'ok'
     });
 });
+
+function PostCode() {
+    // Build the post string from an object
+    var post_data = querystring.stringify({
+        'compilation_level': 'ADVANCED_OPTIMIZATIONS',
+        'output_format': 'json',
+        'output_info': 'compiled_code',
+        'warning_level': 'QUIET'
+    });
+    // An object of options to indicate where to post to
+    var post_options = {
+        host: 'localhost:5280',
+        port: '80',
+        path: '/compile',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Length': Buffer.byteLength(post_data),
+            'Authorization': auth
+        },
+        body:{
+            'newusername':username,
+            'newuserpassword':password,
+            'addnewuser':'Add+User'
+    }
+    };
+
+    // Set up the request
+    var post_req = http.request(post_options, function (res) {
+        res.setEncoding('utf8');
+        res.on('data', function (chunk) {
+            console.log('Response: ' + chunk);
+        });
+    });
+    post_req.write(post_data);
+    post_req.end();
+
+}
+
+
+
+
 var port = process.env.PORT || '3000';
 app.listen(port, function () {
     console.log('listening on port ' + process.env.PORT);
