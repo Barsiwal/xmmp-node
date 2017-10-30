a488218c3b4b8b554ed794a4509a7b0b1a378bb0
Karan Barsiwal <barsiwal.karan@tftus.com>
2017-10-30 10:30:30 +0530

    :asdasdsa

--- app.js
+++ app.js
@@ -26,42 +26,66 @@ app.post("/", function (req, res) {
     var password;
     username = req.body.username;
     password = req.body.password;
-    PostCode();
-    return res.send({
-        status:'ok'
-    });
-});
 
-function PostCode() {
-    // Build the post string from an objectbject of options to indicate where to post to
-    var post_options = {
-        host: 'localhost:5280',
-        port: '80',
-        path: '/compile',
+    const options = {
+        url: 'http://localhost:5280/admin/server/karan-barsiwal-linux/users/',
         method: 'POST',
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded',
-            'Host': 'localhost:5280',
-            'Authorization': auth
+            'Host': 'localhost:5280'
         },
-        body:{
-            'newusername':username,
-            'newuserpassword':password,
-            'addnewuser':'Add+User'
-    }
+        auth: {
+            user: 'admin@karan-barsiwal-linux',
+            password: 'admin'
+        },
+        form: {
+            'newusername': username,
+            'newuserpassword': password,
+            'addnewuser': 'Add+User'
+        }
     };
 
-    // Set up the request
-    var post_req = http.request(post_options, function (res) {
-        res.setEncoding('utf8');
-        res.on('data', function (chunk) {
-            console.log('Response: ' + chunk);
-        });
+    request(options, function (err, res, body) {
+        let json = JSON.parse(body);
+        console.log(json);
     });
-    post_req.write(post_data);
-    post_req.end();
+    return res.send({
+        status: 'ok'
+    });
+});
 
-}
+
+
+//function PostCode() {
+//    // Build the post string from an objectbject of options to indicate where to post to
+//    var post_options = {
+//        host: 'localhost:5280',
+//        port: '80',
+//        path: '/compile',
+//        method: 'POST',
+//        headers: {
+//            'Content-Type': 'application/x-www-form-urlencoded',
+//            'Host': 'localhost:5280',
+//            'Authorization': auth
+//        },
+//        body:{
+//            'newusername':username,
+//            'newuserpassword':password,
+//            'addnewuser':'Add+User'
+//    }
+//    };
+//
+//    // Set up the request
+//    var post_req = http.request(post_options, function (res) {
+//        res.setEncoding('utf8');
+//        res.on('data', function (chunk) {
+//            console.log('Response: ' + chunk);
+//        });
+//    });
+//    post_req.write(post_data);
+//    post_req.end();
+//
+//}
 
 
 
